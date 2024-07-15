e8162e9494f3b571dd1c54ad6a9cddc566e72bc4
Aaron Guo <aguo@mawer.com>
2024-07-14 22:22:37 -0600

    Debug env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompanyModule } from './company/company.module';
+// import { CompanyModule } from './company/company.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-console.log('> process.env', process.env);
-
 @Module({
   imports: [
-    CompanyModule,
+    // CompanyModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
@@ -28,17 +26,6 @@ console.log('> process.env', process.env);
         synchronize: true,
       }),
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.PG_HOST ?? 'localhost',
-    //   port: parseInt(process.env.PG_PORT) ?? 5432,
-    //   username: process.env.PG_USER ?? '',
-    //   password: process.env.PG_PASSWORD ?? '',
-    //   database: process.env.DB ?? '',
-    //   autoLoadEntities: true,
-    //   synchronize: true, // disable in production
-    //   ssl: { rejectUnauthorized: false },
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log('> process.env', process.env);
+    return 'Learn nest.js';
   }
 }
