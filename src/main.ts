53eb8159c5278bb7690f6e3c3f30da7238705854
Aaron Guo <aguo@mawer.com>
2024-07-15 02:01:03 -0600

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,16 @@ async function bootstrap() {
       },
     }),
   );
+  const openAPIOptions = new DocumentBuilder()
+    .setTitle("Aaron Guo Demo's API")
+    .setDescription("API for Aaron Guo's Demo")
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, openAPIOptions);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
